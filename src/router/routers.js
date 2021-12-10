33b67f5d1c01e87e815e4233e07bc2c101bf3a33
李大松 <lids@zhejianglab.com>
2021-12-10 17:34:26 +0800

    feat: framework

---
+++ src/router/routers.js
@@ -0,0 +1,120 @@
+import Main from '@/components/main'
+
+/**
+ * iview-admin中meta除了原生参数外可配置的参数:
+ * meta: {
+ *  title: { String|Number|Function }
+ *         显示在侧边栏、面包屑和标签栏的文字
+ *         使用'{{ 多语言字段 }}'形式结合多语言使用，例子看多语言的路由配置;
+ *         可以传入一个回调函数，参数是当前路由对象，例子看动态路由和带参路由
+ *  hideInBread: (false) 设为true后此级路由将不会出现在面包屑中，示例看QQ群路由配置
+ *  hideInMenu: (false) 设为true后在左侧菜单不会显示该页面选项
+ *  notCache: (false) 设为true后页面在切换标签后不会缓存，如果需要缓存，无需设置这个字段，而且需要设置页面组件name属性和路由配置的name一致
+ *  access: (null) 可访问该页面的权限数组，当前路由设置的权限会影响子路由
+ *  icon: (-) 该页面在左侧菜单、面包屑和标签导航处显示的图标，如果是自定义图标，需要在图标名称前加下划线'_'
+ *  beforeCloseName: (-) 设置该字段，则在关闭当前tab页时会去'@/router/before-close.js'里寻找该字段名对应的方法，作为关闭前的钩子函数
+ * }
+ */
+
+export default [
+  {
+    path: '/',
+    name: '_home',
+    redirect: '/home',
+    component: Main,
+    meta: {
+      hideInMenu: true,
+      notCache: true
+    },
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        meta: {
+          hideInMenu: true,
+          title: '首页',
+          notCache: true,
+          icon: 'md-home',
+          src: 'https://www.yuque.com/explore/headlines'
+        },
+        component: () => import('@/view/single-page/home')
+      }
+    ]
+  },
+  {
+    path: '/components',
+    name: 'components',
+    meta: {
+      icon: 'logo-buffer',
+      title: '组件'
+    },
+    component: Main,
+    children: [
+      {
+        path: 'tree_select_page',
+        name: 'tree_select_page',
+        meta: {
+          icon: 'md-arrow-dropdown-circle',
+          title: '树状下拉选择器',
+          src: 'https://www.yuque.com/about/organization'
+        },
+        component: () => import('@/view/single-page/home')
+      },
+      {
+        path: 'count_to_page',
+        name: 'count_to_page',
+        meta: {
+          icon: 'md-trending-up',
+          title: '数字渐变',
+          src: 'https://www.yuque.com/about/security'
+        },
+        component: () => import('@/view/single-page/home')
+      },
+      {
+        path: 'drag_list_page',
+        name: 'drag_list_page',
+        meta: {
+          icon: 'ios-infinite',
+          title: '拖拽列表',
+          src: 'https://www.yuque.com/about/organization'
+        },
+        component: () => import('@/view/single-page/home')
+      }
+    ]
+  },
+  {
+    path: '/tools_methods',
+    name: 'tools_methods',
+    meta: {
+      hideInBread: true
+    },
+    component: Main,
+    children: [
+      {
+        path: 'tools_methods_page',
+        name: 'tools_methods_page',
+        meta: {
+          icon: 'ios-hammer',
+          title: '工具方法',
+          src: 'https://www.yuque.com/yuque/topics',
+          beforeCloseName: 'before_close_normal'
+        },
+        component: () => import('@/view/single-page/home')
+      }
+    ]
+  }
+]
+
+/**
+ * top nav links
+ */
+export const navs = [{
+  name: '数据工厂',
+  href: 'https://www.baidu.com'
+}, {
+  name: '用力平台',
+  href: 'https://www.baidu.com'
+}, {
+  name: '测试报告',
+  href: 'https://www.baidu.com'
+}]
---
+++ src/view/single-page/home/home.vue
@@ -0,0 +1,31 @@
+<template>
+    <iframe width="100%" style="border: none;" height="94%" :src="src" />
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {},
+  data () {
+    return {
+      src: ''
+    }
+  },
+  watch: {
+    $route (route) {
+      const { meta: { src } } = route
+      this.src = src
+    }
+  },
+  mounted () {
+    const { meta: { src } } = this.$route
+    this.src = src
+  }
+}
+</script>
+
+<style lang="less">
+.count-style{
+  font-size: 50px;
+}
+</style>
